516c1d171481de18b586f0467d05c35dd66f3260
lcarranco <lcarranco@gmail.com>
2018-05-15 17:07:32 -0500

    Added PDF and starting to handle file streams

--- main.cpp
+++ main.cpp
@@ -4,15 +4,37 @@
 
 using namespace std;
 
+
+
+
+void input(BigNumber *a, string filename, int digitsPerNode) {
+    ifstream ifs(filename.c_str());
+
+    int counter = 0;
+    while (!ifs.eof()) {
+        string line;
+        ifs >> line;
+        if (line.size() != 0) {
+            a[counter] = BigNumber(line, digitsPerNode);
+            counter++;
+        }
+    }
+}
+
 int main() {
     string line;
     ifstream ifs("input00.txt");
     ofstream ofs;
+    int num;
+
+    string filename = "input00.txt";
+
+    input();
 
     if (ifs.is_open()) {
-        char c = ifs.get();
+        ifs >> num;
         while (ifs.good()) {
-            cout << c;
+            cout << num;
             c = ifs.get();
             // getline(cin, line);
             // cout << line << endl;
