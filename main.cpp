4efee8dcf6e55c1b1f86a29aee9610fe821ae065
lcarranco <lcarranco@gmail.com>
2018-05-19 11:27:09 -0500

    Pivoted from vector to queue.
    Get number of items ordered
    
    Get index of item to exclude
    
    Get amount Brian charged Anna

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <cstdio>
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <sstream>
 
 using namespace std;
@@ -8,17 +8,34 @@ using namespace std;
 
 int main() {
     string line;
-    vector<int> numbers;
     int value;
+    queue<int> numbers;
     while (getline(cin, line)) {
         stringstream ss(line);
         while (ss >> value) {
-            numbers.push_back(value);
+            numbers.push(value);
         }
     }
 
-    for (vector<int>::iterator it = numbers.begin(); it != numbers.end(); ++it) {
-        cout << *it << ' ';
+    int numberPeoplepaying = 2;
+    int numberItemsOrdered = numbers.front();
+    // cout << numberItemsOrdered << endl;
+    numbers.pop();
+    int indexItemToExclude = numbers.front();
+    // cout << indexItemToExclude << endl;
+    numbers.pop();
+    int amountCharged = numbers.back();
+    // cout << amountCharged << endl;
+    int totalPrice = 0;
+    int excludedItemPrice = 0;
+    int totalPricePerPerson = 0;
+    int correctPricePerPerson = 0;
+    int refund = 0;
+    
+
+    while (!numbers.empty()) {
+        cout << numbers.front() << ' ';
+        numbers.pop();
     }
     cout << endl;
     return 0;
