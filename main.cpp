b5bf3acc0164b7cc65a2265ca531b207624d2ab5
lcarranco <lcarranco@gmail.com>
2018-05-18 20:07:06 -0500

    Reading from cin

--- main.cpp
+++ main.cpp
@@ -1,44 +1,24 @@
 #include <cstdio>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 
-
-
-void input(BigNumber *a, string filename, int digitsPerNode) {
-    ifstream ifs(filename.c_str());
-
-    int counter = 0;
-    while (!ifs.eof()) {
-        string line;
-        ifs >> line;
-        if (line.size() != 0) {
-            a[counter] = BigNumber(line, digitsPerNode);
-            counter++;
-        }
-    }
-}
-
 int main() {
     string line;
-    ifstream ifs("input00.txt");
-    ofstream ofs;
-    int num;
-
-    string filename = "input00.txt";
-
-    input();
-
-    if (ifs.is_open()) {
-        ifs >> num;
-        while (ifs.good()) {
-            cout << num;
-            c = ifs.get();
-            // getline(cin, line);
-            // cout << line << endl;
+    vector<int> numbers;
+    int value;
+    while (getline(cin, line)) {
+        stringstream ss(line);
+        while (ss >> value) {
+            numbers.push_back(value);
         }
+    }
+
+    for (vector<int>::iterator it = numbers.begin(); it != numbers.end(); ++it) {
+        cout << *it;
     }
     cout << endl;
     return 0;
